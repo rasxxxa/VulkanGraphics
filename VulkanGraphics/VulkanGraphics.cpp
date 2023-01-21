0da53439467a011b67fe9a8890941e96926dc18c
radoica nikolic <radoica.nikolic@fazi.rs>
2023-01-21 14:22:35 +0100

    Add project files.

---
+++ VulkanGraphics/VulkanGraphics.cpp
@@ -0,0 +1,12 @@
+﻿// VulkanGraphics.cpp : Defines the entry point for the application.
+//
+
+#include "VulkanGraphics.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake. Test 1 2 3 " << endl;
+	return 1;
+}
